18d7bb8bbbcc0854c936509a71450edec784b877
vibhu mahendru <vibhumahendru@gmail.com>
2018-10-05 15:46:41 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
-def my_select(collection)
- # your code here!
+def my_select(array)
+i = 0 
+newAr = []
+while i<array.length
+if (yield array[i]) == true
+  newAr.push(array[i])
+
+  end
+  i = i + 1
+end
+  newAr
 end
